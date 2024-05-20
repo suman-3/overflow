701513a848872be035a3011b269bbd6528b12b07
Suman Mondal <mondalsumanrock20@gmail.com>
2024-05-21 02:54:25 +0530

    name and logo change

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -9,7 +9,7 @@ import Link from 'next/link'
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Community | Dev Overflow',
+  title: 'Community | Dev Query',
 }
 
 const Page = async ({ searchParams }: SearchParamsProps) => {
